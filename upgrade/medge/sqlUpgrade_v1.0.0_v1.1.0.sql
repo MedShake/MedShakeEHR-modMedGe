cfc2d404ee1a56058ffaa3fbdaac17766e48009c
Bertrand Boutillier <b.boutillier@gmail.com>
2019-01-22 14:58:05 +0100

    fix: sql pour base v5

--- upgrade/medge/sqlUpgrade_v1.0.0_v1.1.0.sql
+++ upgrade/medge/sqlUpgrade_v1.0.0_v1.1.0.sql
@@ -3,5 +3,9 @@ INSERT INTO `configuration` ( `name`, `level`, `toID`, `module`, `cat`, `type`, 
 update `data_types` set label='Règlement', description='Règlement assisté S1' WHERE `name` = 'medGeReglePorteurCalculateur';
 update `data_types` set label='Règlement', description='Règlement conventionné S2' WHERE `name` = 'medGeReglePorteurS2';
 
+-- mise en base du JS des forms 
+update `forms` set `javascript`= '//echos IG : observation nombre foetus\r\n$(\'body\').on(\"keyup, change\", \'#id_igNbFoetus_id\', function() {\r\n  afficherFxNbFoetus();\r\n});\r\n\r\n//issue grossesse : calcul terme à l\'acc\r\n$(\'body\').on(\"focusout\", \'#id_igDate_id\', function() {\r\n  terme = termeAccCalc($(\'#id_igDate_id\').val(), $(\'#id_DDR_id\').val(), $(\'#id_ddgReel_id\').val());\r\n  if (terme != null) {\r\n    $(\'#id_igTermeFA_id, #id_igTermeFB_id, #id_igTermeFC_id\').val(terme);\r\n  }\r\n});'
+where `internalName`='medGeIssueGrossesse';
+
 -- upgrade n° de version
 UPDATE `system` SET `value`='v1.1.0' WHERE `name`='medge';
--- upgrade/medge/sqlUpgrade_v1.0.0_v1.1.0_post.php
+++ upgrade/medge/sqlUpgrade_v1.0.0_v1.1.0_post.php
@@ -1,2 +1,5 @@
 <?php
 @unlink($p['homepath'].'class/msModMedgeCalcHonoraires.php');
+
+// retrait des anciens fichiers JS formulaire
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/medGeIssueGrossesse.js');
