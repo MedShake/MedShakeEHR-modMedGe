b62a43ecaea7def11665b57d241bede2517b62e8
Bertrand Boutillier <b.boutillier@gmail.com>
2019-06-11 11:30:06 +0200

    feat: donotsaveempty appliqué à poids, taille TAS, TAD, FC des forms cs

---
+++ upgrade/medge/sqlUpgrade_v1.3.0_v1.4.0.sql
@@ -0,0 +1,15 @@
+-- upgrade n° de version
+UPDATE `system` SET `value`='v1.4.0' WHERE `name`='medge';
+
+-- ajustement form pour autosize
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- poids,plus={kg} ', '- poids,plus={kg},donotsaveempty ') where internalName in ('medGeConsultPedia2M', 'medGeConsultPedia4M', 'medGeConsultPedia9M', 'medGeConsultPedia24M', 'medGeConsultPedia3A', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- taillePatient,plus={cm} ', '- taillePatient,plus={cm},donotsaveempty ') where internalName in ('medGeConsultPedia2M', 'medGeConsultPedia4M', 'medGeConsultPedia9M', 'medGeConsultPedia24M', 'medGeConsultPedia3A', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- taSystolique,plus={mmHg},class=text-right ', '- taSystolique,plus={mmHg},class=text-right,donotsaveempty ') where internalName in ('medGeConsultAdulte', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- taDiastolique,plus={mmHg},class=text-right ', '- taDiastolique,plus={mmHg},class=text-right,donotsaveempty ') where internalName in ('medGeConsultAdulte', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- taSystolique,plus={mmHg} ', '- taSystolique,plus={mmHg},class=text-right,donotsaveempty ') where internalName in ('medGeConsultAdulte', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- taDiastolique,plus={mmHg} ', '- taDiastolique,plus={mmHg},class=text-right,donotsaveempty ') where internalName in ('medGeConsultAdulte', 'medGeConsultPedia4A', 'medGeConsultPedia6A', 'medGeConsultPedia8a9A', 'medGeConsultPedia11a13A', 'medGeConsultPedia15a16A', 'medGeConsultPedia');
+
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '- freqCardiaque,plus={bpm},class=text-right ', '- freqCardiaque,plus={bpm},class=text-right,donotsaveempty ') where internalName in ('medGeConsultAdulte', 'medGeConsultPedia');
